30e4c757c8da9506db553459dd9e82cdb84d545c
Nataliya R <n.rudenko@gmail.com>
2019-06-25 19:54:22 +0000

    Automatically backed up by Learn

--- app/models/students.rb
+++ app/models/students.rb
@@ -1,8 +1,7 @@
 class Students < ActiveRecord::Base
   def student 
-    first_name = Student.find_by_name
-    last_name = Student.find_by_last_name 
-    (first_name + last_name).to_s
+  
+  (self.first_name + self.last_name).to_s
   end 
     
   
