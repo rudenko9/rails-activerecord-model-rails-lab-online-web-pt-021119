126c1c3f7d267a16fc07b6c927ba860cac280f11
Nataliya R <n.rudenko@gmail.com>
2019-06-25 19:42:07 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -1,8 +1,8 @@
 class CreateStudents < ActiveRecord::Migration
   def change
     create_table :students do |t|
-      t.text :first_name
-      t.text :last_name
+      t.string :first_name
+      t.string :last_name
 end 
   end
 end
